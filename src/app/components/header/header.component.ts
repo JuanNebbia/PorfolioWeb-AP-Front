b89c21411e5c815ace23e184df3b35e81b9a4ac5
Juan Nebbia <juan.nebbia@gmail.com>
2023-08-22 17:18:53 -0300

    Finish information update modal

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Informacion } from 'src/app/models/informacion';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 import { HeaderService } from 'src/app/services/header.service';
 
 @Component({
@@ -11,9 +12,11 @@ import { HeaderService } from 'src/app/services/header.service';
 export class HeaderComponent implements OnInit {
 
   public informacion:Informacion | undefined;
+  public updateInfo: Informacion | undefined;
   public loading:boolean = true;
+  userLogged = this.authenticationService.getLoggedUser();
 
-  constructor(private headerService:HeaderService) { }
+  constructor(private headerService:HeaderService, private authenticationService:AuthenticationService) { }
 
   ngOnInit(): void {
     this.getInformacion();
@@ -31,5 +34,24 @@ export class HeaderComponent implements OnInit {
       complete: () => this.loading = false
     });
   }
+
+  
+  public onOpenModal():void{
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.style.display='none';
+    button.setAttribute('data-toggle', 'modal');
+      this.updateInfo=this.informacion;
+      button.setAttribute('data-target', '#updateHabilidadesModal')
+    container?.appendChild(button);
+    button.click();
+  }
+ 
+  public onUpdateInfo(informacion:Informacion){
+    this.updateInfo = informacion;
+    document.getElementById("update-info-form")?.click();
+    this.headerService.updateInformacion(informacion)
+  }
+
 
 }
